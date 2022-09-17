6a5510c87b6567fbd6ae8a8dd71778ab68dab41f
Aniket <aniketkumarpaul1@gmail.com>
2022-09-17 19:13:36 +0530

    Basic setup and files

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+// to ease importing components from just one file 
+export {default as Loader} from './Loader';
+export {default as Navbar} from './Navbar';
+export {default as Footer} from './Footer';
+export {default as Welcome} from './Welcome';
+export {default as Transactions} from './Transactions';
+export {default as Services} from './Services';
