3ad67c237d22c0efbdf7453a012bba60c2b9d120
Aniket <aniketkumarpaul1@gmail.com>
2022-09-19 15:25:46 +0530

    added deploy.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/OZzEVoTgx344HjELFerS43IaMUBABVa6',
+      accounts: ['e6d8f40783f6af5d934d9898c14a58127a03d060d481bd5d46fa94d14768d264']
+    }
+  }
 };
